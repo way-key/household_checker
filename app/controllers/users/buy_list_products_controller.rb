33b7ff380aa25b81f052719eb44151ce291682c0
way-key <45gqz7@gmail.com>
2020-06-02 15:24:32 +0900

    [Update]買い物リスト機能の修正

--- app/controllers/users/buy_list_products_controller.rb
+++ app/controllers/users/buy_list_products_controller.rb
@@ -2,7 +2,6 @@ class Users::BuyListProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_buy_list_product, only: [:edit, :update, :destroy]
 
-
   def create
     @buy_list_product = BuyListProduct.new(buy_list_product_params)
     @buy_list_product.save
@@ -11,7 +10,15 @@ class Users::BuyListProductsController < ApplicationController
   end
 
   def update
-    @buy_list_product.update(buy_list_product_params)
+    dead_line = @buy_list_product.dead_line
+    if params[:buy_list_product][:cost].present?
+      update_dead_line = dead_line + ((params[:buy_list_product][:cost].to_i * params[:buy_list_product][:amount].to_i).days)
+    else
+      update_dead_line = Date.today
+    end
+    new_buy_list_product_params = buy_list_product_params
+    new_buy_list_product_params[:dead_line] = update_dead_line
+    @buy_list_product.update(new_buy_list_product_params)
     redirect_back(fallback_location: root_path)
     # 元のページへリダイレクト
   end
@@ -28,7 +35,7 @@ class Users::BuyListProductsController < ApplicationController
   end
 
   def buy_list_product_params
-    params.require(:buy_list_product).permit(:buy_list_id, :product_id, :amount, :last_buy, :last_amount, :cost, :dead_line)
+    params.require(:buy_list_product).permit(:buy_list_id, :product_id, :amount, :cost, :dead_line)
   end
 
 end
