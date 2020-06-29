42f7dd96ae5550f8ef86069501c46612872f0daa
way-key <45gqz7@gmail.com>
2020-06-29 14:47:36 +0900

    [Update]お気に入りボタンの修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,7 +3,7 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  def product_by?(user)
+  def favorited_by?(user)
     Favorite.where(user_id: user.id).exists?
   end
 
