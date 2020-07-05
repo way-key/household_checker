ceb9fa494d7f8538abb9619101b29abb760c92c3
way-key <45gqz7@gmail.com>
2020-07-05 17:20:33 +0900

    [Finish]cronでのメール連絡機能実装

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,11 @@
 class NotificationMailer < ApplicationMailer
   def send_when_check
-    @check_buy_list = BuyListProduct.where(dead_line: 3.days.since)
-    @user = User.where(id: @check_buy_list.buy_list.user.id)
-    if @check_buy_list.present?
-      mail to: @user.map(&:email).join(", "),
-      # 買い物リスト商品の中で、有効期限が三日前に迫ったユーザーが宛先
+    @check_dead_line = User.eager_load(buy_lists: :buy_list_products).where(buy_list_products: { dead_line: 3.days.since.strftime("%Y-%m-%d") })
+    # 期限が迫った買い物リスト商品を持ってるユーザーを抽出
+    if @check_dead_line.present?
+      mail to: @check_dead_line.map(&:email).join(","),
+      # 変数のユーザ全員を宛先
       subject: "買い物リスト商品の期限が近づいてきました"
     end
   end
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,15 +92,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'smtp.gmail.com',
-    user_name:            ENV['GMAIL_MAIL'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'login',
-    enable_starttls_auto: true
-    }
 end
